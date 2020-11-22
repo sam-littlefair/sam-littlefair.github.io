191b119353e1e65b4fabacc554e6da266ed6db17
Sam Littlefair <slittlefair@bloomberg.net>
2020-11-22 18:59:10 +0000

    Remove autoupdate viewport

--- js/main.js
+++ js/main.js
@@ -82,11 +82,4 @@ $(function () {
     if (scrollAnim) {
         new WOW().init();
     }
-
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-    });
 });
