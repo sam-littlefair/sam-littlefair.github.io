68d027027cce1fc7ab81efd9a8dd9c86c06fb7c0
Sam Littlefair <slittlefair@bloomberg.net>
2020-11-22 18:54:04 +0000

    Fix viewport with JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function () {
     "use strict";
-
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     if ($(".menu-items").length) {
         var defaultFilter = $(".tagsort-active").attr("data-filter");
         var $grid = $(".menu-items").isotope({
@@ -79,4 +82,11 @@ $(function () {
     if (scrollAnim) {
         new WOW().init();
     }
+
+    // We listen to the resize event
+    window.addEventListener('resize', () => {
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 });
